2c471b845fe8d5f99569a3fe085c02f97f51291d
Akseli N <virtualakseli@gmail.com>
2020-11-02 13:39:12 +0200

    NhlStats kuntoon

---
+++ viikko1/NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+/**
+ *
+ * @author aknu
+ */
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StatisticsTest {
+
+    Reader readerStub = new Reader() {
+        @Override
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri", "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+            players.add(new Player("Malkin", "PIT", 8, 19));
+
+            return players;
+        }
+
+    };
+
+    Statistics stats;
+
+    @Before
+    public void setUp() {
+
+        stats = new Statistics(readerStub);
+
+    }
+    
+    @Test 
+    public void playerSearchworks1() {
+        
+        assertEquals(null, stats.search("Nemo"));
+    }
+    
+    @Test 
+    public void playerSearchworks2() {
+        
+        assertEquals("Gretzky", stats.search("Gretzky").getName());
+    }
+
+    @Test
+    public void playerPointsCorrect1() {
+        assertEquals(98, stats.search("Yzerman").getPoints());
+    }
+
+    @Test
+    public void teamSearchWorks1() {
+        List<Player> l = stats.team("PIT");
+
+        assertEquals(2, l.size());
+
+    }
+
+    @Test
+    public void teamSearchWorks2() {
+        List<Player> l = stats.team("NIL");
+
+        assertEquals(0, l.size());
+
+    }
+
+    @Test
+    public void topScorers() {
+        assertEquals("Gretzky", stats.topScorers(1).get(0).getName().trim());
+    }
+
+    
+
+}
